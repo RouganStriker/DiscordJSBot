a3e7c6463c564f7d78efb85f23ec78a8f4af1566
Kelvin Chan <kelvinc.25@gmail.com>
2016-11-16 23:18:19 -0500

    Bind this

--- utils/Command.js
+++ utils/Command.js
@@ -1,9 +1,9 @@
 class Command {
   constructor(name, description, callback=null, permissions=[]) {
-    this.name = name;                               // Command name
-    this.description = description;                 // Command description
-    this.callback = callback || this.runCommand;    // Optional. Function to call on match
-    this.permissions = permissions;                 // Optional. Required user permissions to see and use this command
+    this.name = name;                                          // Command name
+    this.description = description;                            // Command description
+    this.callback = callback || this.runCommand.bind(this);    // Optional. Function to call on match
+    this.permissions = permissions;                            // Optional. Required user permissions to see and use this command
   }
 
   runCommand() {
